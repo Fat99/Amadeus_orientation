9e23f827b0e48a4506c89af7f80d341c426759af
Eldar Fattakhov <fattakhov.em@mipt.ru>
2021-09-29 14:34:51 +0300

    Push вместе с моментом импульса маховиков

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ t_0, nu = 0, 6.809e15
 # для Земли
 # t_0, e, nu = 0, 0.01, 398600.4415 * 10 ** 9
 
-t_final = 1000
-t = np.linspace(0, t_final, int(0.8 * t_final + 1))  # для Нептуна
+# t_final = 250000
+t_final = 80000
+
+t = np.linspace(0, t_final, t_final + 1)  # для Нептуна
 # t = np.linspace(0, 5400, 5401)  # для Земли
 
 # Для Нептуна
@@ -31,18 +33,19 @@ r_and_v = odeint(traj_and_speed, r_and_v0, t, args=(nu,))
 r, v = np.zeros((t.size, 3)), np.zeros((t.size, 3))
 r[:], v[:] = r_and_v[:, :3], r_and_v[:, 3:]
 
-high, massa, radius = 7, 3820, 2.5
-J_x, J_z = 1 / 12 * massa * (3 * radius ** 2 + high ** 2), massa * radius ** 2 / 2
 
-j_tenzor = np.array([[J_z, 0, 0],
-                     [0, J_x, 0],
-                     [0, 0, J_x]])
+j_tenzor = np.array([[3348, 0, 0],
+                     [0, 1836, 0],
+                     [0, 0, 4548]])
 
+# print(j_tenzor)
 # k_w_and_q = [[0.7, 0.01], [0.65, 0.008], [0.75, 0.02], [0.6, 0.02]]
-w_rel_init, quat0 = np.array([0, 0, 0.01]), np.array([0, 0, np.sin(np.pi / 20), np.cos(np.pi / 20)])
+w_rel_init, quat0 = np.array([0, 0, 0]), np.array([0, 0, 0, 1])
 w_rel_and_quat0 = np.concatenate([w_rel_init, quat0])
 
-k_w, k_q = 0.85, 0.04
+# k_w, k_q = 0.025, 0.0015
+k_w, k_q = 1.2, 0.01
+
 
 w_rel_and_quat = odeint(w_rel_and_quat, w_rel_and_quat0, t, args=(j_tenzor, k_w, k_q))
 
@@ -69,15 +72,15 @@ for i in range(0, t.size):
 
     w_ref_t[i] = w_ref(p, t[i], t_0, r[i])
 
-# plt.plot(t / 3600, M_ctrl[:, 0], label='M_ctrl_x')
-# plt.plot(t / 3600, M_ctrl[:, 1], label='M_ctrl_y')
-# plt.plot(t / 3600, M_ctrl[:, 2], label='M_ctrl_z')
-# plt.legend(loc='best')
-# plt.title('График зав-ти управляющего момента M_ctrl от времени')
-# plt.xlabel('t, час')
-# plt.ylabel('M_ctrl, Н*м')
-# plt.grid()
-# plt.show()
+plt.plot(t / 3600, M_ctrl[:, 0], label='M_ctrl_x')
+plt.plot(t / 3600, M_ctrl[:, 1], label='M_ctrl_y')
+plt.plot(t / 3600, M_ctrl[:, 2], label='M_ctrl_z')
+plt.legend(loc='best')
+plt.title('График зав-ти управляющего момента M_ctrl от времени')
+plt.xlabel('t, час')
+plt.ylabel('M_ctrl, Н*м')
+plt.grid()
+plt.show()
 
 # plt.plot(t / 3600, w_rel[:, 0], label='w_rel_x')
 # plt.plot(t / 3600, w_rel[:, 1], label='w_rel_y')
@@ -88,7 +91,19 @@ for i in range(0, t.size):
 # plt.ylabel('w_rel, c^-1')
 # plt.grid()
 # plt.show()
-#
+
+plt.plot(t / 3600, quat[:, 0], label='quat_x')
+plt.plot(t / 3600, quat[:, 1], label='quat_y')
+plt.plot(t / 3600, quat[:, 2], label='quat_z')
+plt.plot(t / 3600, quat[:, 3], label='quat_scalar')
+plt.legend(loc='best')
+plt.title('График зав-ти quat от времени')
+plt.xlabel('t, час')
+plt.ylabel('quat')
+plt.grid()
+plt.show()
+
+
 # plt.plot(t / 3600, w_abs[:, 0], label='w_abs_x')
 # plt.plot(t / 3600, w_abs[:, 1], label='w_abs_y')
 # plt.plot(t / 3600, w_abs[:, 2], label='w_abs_z')
@@ -110,26 +125,35 @@ for i in range(0, t.size):
 # plt.show()
 
 
-t_len = t.size
-
-
 # уравнение на моменты импульса маховиков
-def h_machs(y, t):
-    h_mach = y[:3]
-    dh_dt = np.zeros(3)
-    print('mom_ctrl_check = ', M_ctrl[int(t / t_final * t_len - 1), :])
-    print('cross = ', np.cross(w_abs[int(t / t_final * t_len - 1), :], h_mach))
-    dh_dt[:] = -M_ctrl[int(t / t_final * t_len - 1), :] - np.cross(w_abs[int(t / t_final * t_len - 1), :], h_mach)
-    return dh_dt
+# def h_machs(y, t):
+#     h_mach = y[:3]
+#     dh_dt = np.zeros(3)
+#     # print('mom_ctrl_check = ', M_ctrl[int(t / t_final * t_len - 1), :])
+#     # print('cross = ', np.cross(w_abs[int(t / t_final * t_len - 1), :], h_mach))
+#     print(int(t / t_final * t_len) - 1)
+#     dh_dt[:] = -M_ctrl[int(t / t_final * t_len) - 1, :] - np.cross(w_abs[int(t / t_final * t_len) - 1, :], h_mach)
+#     return dh_dt
+#
+#
+#
+# h_mach_init = np.zeros(3)
+#
+# h_mach = odeint(h_machs, h_mach_init, t)
 
-
+# t_len = t.size
+t_len, dt = t.size, t[1] - t[0]
 h_mach_init = np.zeros(3)
+H_mach = np.zeros((t_len, 3))
+h_mach = h_mach_init
+for i in range(0, t.size):
+    dhdt = -M_ctrl[i] - np.cross(w_abs[i], h_mach)
+    h_mach = h_mach + dhdt * dt
+    H_mach[i][:] = h_mach
 
-h_mach = odeint(h_machs, h_mach_init, t)
-
-plt.plot(t / 3600, h_mach[:, 0], label='h_mach_x')
-plt.plot(t / 3600, h_mach[:, 1], label='h_mach_y')
-plt.plot(t / 3600, h_mach[:, 2], label='h_mach_z')
+plt.plot(t / 3600, H_mach[:, 0], label='h_mach_x')
+plt.plot(t / 3600, H_mach[:, 1], label='h_mach_y')
+plt.plot(t / 3600, H_mach[:, 2], label='h_mach_z')
 plt.legend(loc='best')
 plt.title('График зав-ти h_mach от времени')
 plt.xlabel('t, час')
